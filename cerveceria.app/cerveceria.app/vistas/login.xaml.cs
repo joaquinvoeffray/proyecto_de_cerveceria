a2847a88cd914049fc831e68a890a2b9213f57a0
joaquinvoeffray <joaquinv1234@hotmail.com>
2023-11-12 07:04:04 -0300

    cargar carpeta dexamarin

---
+++ cerveceria.app/cerveceria.app/vistas/login.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cerveceria.app.vistas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class login : ContentPage
+	{
+		public login ()
+		{
+			InitializeComponent ();
+			//aca tendria que mostrar cambios
+		}
+	}
+}
